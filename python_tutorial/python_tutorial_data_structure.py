06cca68850899f5e10c37855b3d0a3a46a2543b9
L. Li <leeo1116@gmail.com>
2015-06-05 15:26:49 -0700

    list as stack

--- python_tutorial/python_tutorial_data_structure.py
+++ python_tutorial/python_tutorial_data_structure.py
@@ -1,1 +1,12 @@
 __author__ = 'Liang Li'
+
+# Use list as stack
+lst = [12, 'asd', [12, 'asd'], {"age": 10, "letter": 'a'}]
+lst.append(10)
+lst.append('a')
+print(lst)
+lst.pop()
+lst.pop()
+print(lst)
+
+# Use list as queue
