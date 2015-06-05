32544d6591608e4842c391397450b7ad94713a9e
L. Li <leeo1116@gmail.com>
2015-06-05 16:47:18 -0700

    list as stack

--- python_tutorial/python_tutorial_data_structure.py
+++ python_tutorial/python_tutorial_data_structure.py
@@ -1,5 +1,5 @@
 __author__ = 'Liang Li'
-
+from collections import deque
 # Use list as stack
 lst = [12, 'asd', [12, 'asd'], {"age": 10, "letter": 'a'}]
 lst.append(10)
@@ -10,3 +10,21 @@ lst.pop()
 print(lst)
 
 # Use list as queue
+queue = deque(["Alice", "Boc", "Evil"])
+queue.append("Liang")
+queue.append("Li")
+print(queue)
+queue.popleft()
+print(queue)
+
+# List comprehensions / literals
+sqr = []
+for x in range(10):
+    sqr.append(x**2)
+print(sqr)
+
+sqr = list(map(lambda x: x**2, range(10)))
+print(sqr)
+sqr = [x**2 for x in range(10)]
+print(sqr)
+print([(x, y) for x in [1, 2, 3] for y in [3, 1, 4] if x != y])
