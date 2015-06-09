f32bfa9bbc4baff5bcbe686b0134c0d3da1803ab
L. Li <leeo1116@gmail.com>
2015-06-08 23:52:42 -0700

    python official documentation

--- python_tutorial/python_tutorial_data_structure.py
+++ python_tutorial/python_tutorial_data_structure.py
@@ -31,3 +31,33 @@ print([(x, y) for x in [1, 2, 3] for y in [3, 1, 4] if x != y])
 
 freshfruit = [' banana', ' loganberry ', 'passion fruit ']
 print([weapon.strip() for weapon in freshfruit])
+
+vec = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print([num for elem in vec for num in elem])
+
+from math import pi
+print([str(round(pi, i)) for i in range(1, 6)])
+
+# Nested list comprehensions/literals
+matrix = [[1, 2, 3, 4],
+          [5, 6, 7, 8],
+          [9, 10, 11, 12]]
+print([[row[i] for row in matrix] for i in range(4)])
+
+# alternative way 1
+transposed = []
+for i in range(4):
+    transposed.append([row[i] for row in matrix])
+print(transposed)
+
+transposed = []
+# alternative way 2
+for i in range(4):
+    transposed_row = []
+    for row in matrix:
+        transposed_row.append(row[i])
+    transposed.append(transposed_row)
+
+# builtin function -- zip()
+print(list(zip(*matrix)))
+
