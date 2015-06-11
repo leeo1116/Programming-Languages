31ff80566e6b98c67f5b3696a1e1e54f8a4dcbf7
L. Li <leeo1116@gmail.com>
2015-06-10 17:41:29 -0700

    sets, dictionary

--- python_tutorial/python_tutorial_data_structure.py
+++ python_tutorial/python_tutorial_data_structure.py
@@ -95,5 +95,4 @@ print(dict({('sape', 4139), ('guido', 4127), ('jack', 4098)}))
 dict1 = {x: x**2 for x in (2, 4, 6)}
 print(dict1)
 print(dict1[4])
-
-
+print(dict(sape=4139, guido=4127, jack=4098))
