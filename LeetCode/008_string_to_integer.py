0bdc92181fce018879c37e229b56470d50641d3b
L. Li <leeo1116@gmail.com>
2015-07-17 13:23:51 -0700

    008_string_to_integer

--- LeetCode/008_string_to_integer.py
+++ LeetCode/008_string_to_integer.py
@@ -4,36 +4,42 @@ class Solution:
     # @return {integer}
     def myAtoi(self, str):
         integer_str = ""
-        neg_flag = 0
-        pos_flag = 0
-        integer = 0
-        for c in str:
-            if (integer_str != "" or neg_flag+pos_flag > 0) and (not c.isdigit()):
-                if integer_str == "":
+        sign = 0
+        min_int, max_int = -1 << (32-1), (1 << (32-1))-1
+        last_i = 0
+        for i in range(len(str)):
+            if integer_str == "" and (not (ord('0') <= ord(str[i]) <= ord('9') or str[i] in {'+', '-'})) and str[i] != ' ':
+                return 0
+            if integer_str == "" and (not ord('0') <= ord(str[i]) <= ord('9')) and sign != 0:
+                return 0
+            if (integer_str != "" or sign != 0) and (not ord('0') <= ord(str[i]) <= ord('9')):
+                if sign == 0:
+                    sign = 1
+                integer = int(integer_str)*sign if integer_str != "" else 0
+                return (integer > max_int)*max_int + (integer < min_int)*min_int + (min_int <= integer <= max_int)*integer if str != "" else 0
+            if str[i] == '-':
+                if sign == 1:
                     return 0
-                else:
-                    integer = int(integer_str)*(1-2*neg_flag)
-                    return integer if 2**(32-1)-1 >= integer >= -2**(32-1) else 0
-            if c == '-':
-                neg_flag = 1
-            if c == '+':
-                pos_flag = 1
-            if c.isdigit():
-                integer_str += c
-        if pos_flag and neg_flag:
-            return 0
-        if integer_str != "":
-            integer = int(integer_str)*(1-2*neg_flag)
-            if 2**(32-1)-1 >= integer >= -2**(32-1):
-                return integer
-            elif integer > 2**(32-1)-1:
-                return 2**(32-1)-1
-            else:
-                return -2**(32-1)
-        else:
-            return 0
+                sign = -1
+            if str[i] == '+':
+                if sign == -1:
+                    return 0
+                sign = 1
+
+            if ord('0') <= ord(str[i]) <= ord('9'):
+                if (i - last_i) > 1:
+                    return 0
+                integer_str += str[i]
+            last_i = i
+        if sign == 0:
+            sign = 1
+
+        integer = int(integer_str)*sign if integer_str != "" else 0
+        return (integer > max_int)*max_int + (integer < min_int)*min_int + (min_int <= integer <= max_int)*integer if str != "" else 0
+
+
 
 
 s = Solution()
-i = s.myAtoi("   - 321")
+i = s.myAtoi(" +1")
 print(i)
