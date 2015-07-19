35bfd25e2e537edce90da58d1ac1426566857ccc
L. Li <leeo1116@gmail.com>
2015-07-19 16:58:45 -0700

    010_regular_expression_matching

--- LeetCode/010_regular_expression_matching.py
+++ LeetCode/010_regular_expression_matching.py
@@ -3,7 +3,7 @@ class Solution:
     # @param {string} s
     # @param {string} p
     # @return {boolean}
-    def isMatch(self, s, p):
+    def regular_expression_matching(self, s, p):
         if not p:
             return not s
         """
@@ -18,15 +18,15 @@ class Solution:
         """
         if p[-1] == '*':
             if s and (s[-1] == p[-2] or p[-2] == '.'):
-                return self.isMatch(s, p[:-2]) or self.isMatch(s[:-1], p)  # Case1.1 or (Case1.2, Case1.3)
+                return self.regular_expression_matching(s, p[:-2]) or self.regular_expression_matching(s[:-1], p)  # Case1.1 or (Case1.2, Case1.3)
             else:
-                return self.isMatch(s, p[:-2])
+                return self.regular_expression_matching(s, p[:-2])
         else:
             if s and (s[-1] == p[-1] or p[-1] == '.'):
-                return self.isMatch(s[:-1], p[:-1])
+                return self.regular_expression_matching(s[:-1], p[:-1])
             else:
                 return False
 
 s = Solution()
-is_matched = s.isMatch("abb", "abb*")
+is_matched = s.regular_expression_matching("abb", "abb*")
 print(is_matched)
