23b44cf5fb81b8ce4da00f197515f4b1014fa8a9
L. Li <leeo1116@gmail.com>
2015-07-21 17:21:41 -0700

    017_letter_combinations

--- LeetCode/017_letter_combinations_of_a_phone_number.py
+++ LeetCode/017_letter_combinations_of_a_phone_number.py
@@ -3,12 +3,16 @@ class Solution:
     # @param {string} digits
     # @return {string[]}
     def letterCombinations(self, digits):
-        digit_letter_map = {'1': '', '2': "abc", '3': "def", '4': "ghi", '5': "jkl", '6': "mno", '7': "pqrs", '8': "tuv",
-                            '9': "wxyz", '0': ''}
-        letter, i = [' ']*len(digits), 0
+        dict = {"1":"", "2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs","8":"tuv","9":"wxyz","10":" "}
+        result = [""]
         for digit in digits:
-            letter[i] = digit_letter_map[digit]
-            i += 1
+            lst = dict[digit]
+            newresult = []
+            for char in lst:
+                for str in result:
+                    newresult.append(str+char)
+            result = newresult
+        return result
 
 s = Solution()
-s.letterCombinations('213')
+print(s.letterCombinations('234'))
