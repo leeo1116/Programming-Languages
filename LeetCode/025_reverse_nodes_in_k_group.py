db2fc035018889e3df459cb683a57e2e86b34484
L. Li <leeo1116@gmail.com>
2015-08-17 17:31:42 -0700

    08/17

--- LeetCode/025_reverse_nodes_in_k_group.py
+++ LeetCode/025_reverse_nodes_in_k_group.py
@@ -1,1 +1,28 @@
 __author__ = 'Liang Li'
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    # @param {ListNode} head
+    # @param {integer} k
+    # @return {ListNode}
+    def reverseKGroup(self, head, k):
+        h = head
+        if not h.next:
+            return h
+
+        while h.next:
+            h_tmp = h
+            p = h.next
+            for counter in range(k, 1):
+                if h.next:
+                    q = p.next
+                    p.next = h
+                    h = p
+                    p = q
+            h = p
+            h_tmp.next = p
+
