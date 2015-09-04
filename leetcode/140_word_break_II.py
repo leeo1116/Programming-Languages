780596f4be57359622285ecc8dca38cb73601b30
L. Li <leeo1116@gmail.com>
2015-09-03 17:25:07 -0700

    09/03

--- leetcode/140_word_break_II.py
+++ leetcode/140_word_break_II.py
@@ -8,17 +8,7 @@ class Solution(object):
         :type wordDict: Set[str]
         :rtype: List[str]
         """
-        if not s:
-            return []
-        valid_word = []
-        valid_words = []
-        for i in range(len(s)):
-            for word in wordDict:
-                if word == s[:i+1]:
-                    valid_word = [word]+self.wordBreak(s[i+1:], wordDict)
 
-            valid_words += valid_word
-            return valid_words
 
 s = Solution()
 print(s.wordBreak("catsanddoge", ["cat", "cats", "and", "sand", "dog"]))
