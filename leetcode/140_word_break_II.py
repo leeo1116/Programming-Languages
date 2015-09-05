9c540993c09ea905d644ec9f200a923305b22c08
L. Li <leeo1116@gmail.com>
2015-09-04 22:39:14 -0700

    09/04

--- leetcode/140_word_break_II.py
+++ leetcode/140_word_break_II.py
@@ -1,6 +1,4 @@
 __author__ = 'liangl2'
-
-
 class Solution(object):
     def wordBreak(self, s, wordDict):
         """
@@ -8,8 +6,29 @@ class Solution(object):
         :type wordDict: Set[str]
         :rtype: List[str]
         """
+        self.dict = wordDict
+        self.cache = {}
+        return self.break_helper(s)
 
+    def break_helper(self, s):
+        combs = []
+        if s in self.cache:
+            return self.cache[s]
+        if len(s) == 0:
+            return []
+
+        for i in range(len(s)):
+            if s[:i+1] in self.dict:
+                if i == len(s) - 1:
+                    combs.append(s[:i+1])
+                else:
+                    sub_combs = self.break_helper(s[i+1:])
+                    for sub_comb in sub_combs:
+                        combs.append(s[:i+1] + ' ' + sub_comb)
+
+        self.cache[s] = combs
+        return combs
 
 s = Solution()
-print(s.wordBreak("catsanddoge", ["cat", "cats", "and", "sand", "dog"]))
+print(s.wordBreak("aaaaaaa", ["aaaa","aaa"]))
 
