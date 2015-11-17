4b3796aeca16ed8de0b41a7a16bbd95a37a5aa6f
L. Li <leeo1116@gmail.com>
2015-11-16 22:11:56 -0800

    11/16

--- leetcode_charlie/021_merge_two_sorted_list.py
+++ leetcode_charlie/021_merge_two_sorted_list.py
@@ -15,5 +15,54 @@ class Solution(object):
         pass
 
     def merge_two_sorted_list(self, l1, l2):
-        pass
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        h = l = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                l.next = l1
+                l1 = l1.next
+            else:
+                l.next = l2
+                l2 = l2.next
+            l = l.next
+        if l1:
+            l.next = l1
+        elif l2:
+            l.next = l2
+        return h.next
 
+
+head1 = ListNode(0)
+l1 = head1
+list_value1 = [1, 2, 5]
+for i in list_value1:
+    l1.next = ListNode(i)
+    l1 = l1.next
+
+head2 = ListNode(0)
+l2 = head2
+list_value2 = [3, 4]
+for i in list_value2:
+    l2.next = ListNode(i)
+    l2 = l2.next
+
+l1 = head1.next
+l2 = head2.next
+
+while l1:
+    print(l1.val, end=', ')
+    l1 = l1.next
+print()
+while l2:
+    print(l2.val, end=', ')
+    l2 = l2.next
+print()
+s = Solution()
+h = s.merge_two_sorted_list(head1.next, head2.next)
+
+while h:
+    print(h.val, end=', ')
+    h = h.next
