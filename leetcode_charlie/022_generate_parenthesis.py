f071cb8afbd8148060a554181e29b696ec99e990
L. Li <leeo1116@gmail.com>
2015-11-18 15:52:14 -0800

    11/18

--- leetcode_charlie/022_generate_parenthesis.py
+++ leetcode_charlie/022_generate_parenthesis.py
@@ -11,6 +11,31 @@ class Solution(object):
     def __init__(self):
         pass
 
-    def generate_parenthesis(self, n):
-        left = right = n
+    def generateParenthesis(self, n):
+        return self.generate_parenthesis(n, n, '', [])
+
+    def generate_parenthesis(self, left, right, p, parenthesis):
+        """
+        General backtracking algorithm.
+        Use left, right to denote the number of remaining left and right parenthesis respectively
+        Use p to store each possible parenthesis combination, parenthesis to store all the combinations
+
+        :param left:
+        :param right:
+        :param p:
+        :param parenthesis:
+        :return:
+        """
+        if not right:
+            parenthesis.append(p)
+        if left:
+            self.generate_parenthesis(left-1, right, p+'(', parenthesis)
+        if right > left:
+            self.generate_parenthesis(left, right-1, p+')', parenthesis)
+        return parenthesis
+
+
+s = Solution()
+print(s.generateParenthesis(2))
+
 
