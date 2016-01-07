776360e2a9aff6d441fd93f4968f60c88bff9be1
L. Li <leeo1116@gmail.com>
2016-01-06 21:43:37 -0800

    01/06 Good luck

---
+++ intern/temp/neighbor_team.py
@@ -0,0 +1,22 @@
+__doc__ = """
+Problem 1: Given non-duplicating list A, B and C. Determine which elements appear in both A and B but not in C
+
+Problem 2: You have a list of integers. Count the number of times each integer appears in the list
+
+Problem 3: Extract the name/value pairs from the following string and put into hash user_id(lambert), license_id(999999),
+balance(100)
+"""
+
+
+class Solution(object):
+    def __init__(self):
+        pass
+
+    def elements_occurance_in_lists(self, list_a, list_b, list_c):
+        return
+
+    def count_number(self, nums):
+        return
+
+    def extract_name_value_pair(self, s):
+        return
---
+++ leetcode_charlie/028_implement_strStr().py
@@ -0,0 +1,32 @@
+__doc__  = """Implement strStr(). Returns the index of the first occurrence of needle in haystack, or -1 if needle is
+ not part of haystack."""
+
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        h_len = len(haystack)
+        n_len = len(needle)
+        if not h_len or not n_len:
+            return 0 if h_len == n_len else -1
+
+        for i in range(h_len):
+            j = 0
+            for j in range(n_len):
+                if i+j >= h_len:
+                    return -1
+                if haystack[i+j] != needle[j]:
+                    break
+            if j == n_len-1 and needle[j] == haystack[i+j]:
+                return i
+        return -1
+
+
+
+
+
+
