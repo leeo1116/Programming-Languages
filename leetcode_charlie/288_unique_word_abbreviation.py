0e3b83be5f0f785cfec0e953064a0e9adf74bb57
L. Li <leeo1116@gmail.com>
2016-02-04 20:06:30 -0600

    02/04

---
+++ leetcode_charlie/288_unique_word_abbreviation.py
@@ -0,0 +1,7 @@
+class UniqueWordAbbreviation(object):
+    def __init__(self, dictionary):
+        """
+        :type dictionary: List[str]
+        :param dictionary:
+        :return: None
+        """
