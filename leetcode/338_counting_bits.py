8506a11a7d96162bc6b0aaf0e09a3f3edf62a6f5
Charlie <leeo1116@gmail.com>
2016-05-24 14:12:01 -0500

    05/24

--- leetcode/338_counting_bits.py
+++ leetcode/338_counting_bits.py
@@ -1,1 +1,19 @@
-https://leetcode.com/problems/counting-bits/
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        count = [0]*(num+1)
+        j = 0
+
+        for i in range(1, num+1):
+            k = i & (2**j-1)
+            if i == 2**(j+1)-1:
+                j += 1
+            count[i] = count[k] + 1
+        return count
+
+
+s = Solution()
+print(s.countBits(3))
