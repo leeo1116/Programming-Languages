9c0f10e8758a22b5c151224ff8f382a58886591c
Charlie <leeo1116@gmail.com>
2016-05-28 20:29:47 -0500

    05/28

--- leetcode/327_count_of_range_sum.py
+++ leetcode/327_count_of_range_sum.py
@@ -7,13 +7,13 @@ class Solution(object):
         :rtype: int
         """
         nums_len = len(nums)
-        range_sum = [0]*(nums_len+1)
+        range_sum = [0]*(nums_len+1)  # why nums_len+1 instead of nums_len
         for i in range(nums_len):
             range_sum[i+1] = range_sum[i]+nums[i]
-        return self.count_while_merge_sort(range_sum, 0, nums_len+1, lower, upper)
+        return self.count_while_merge_sort(range_sum, 0, nums_len+1, lower, upper)  # why nums_len+1
 
     def count_while_merge_sort(self, sums, start, end, lower, upper):
-        if end-start <= 1:
+        if end-start <= 1:  # why <= 1 instead of < 0 or <= 0
             return 0
         mid = start+(end-start)//2
         count = self.count_while_merge_sort(sums, start, mid, lower, upper)+self.count_while_merge_sort(sums, mid, end,
@@ -22,28 +22,25 @@ class Solution(object):
         range_sum_temp = [0]*(end-start)
         m = 0
         for n in range(start, mid):
+            # Merge sort
+            while k < end and sums[k] < sums[n]:  # right part[k] is smaller
+                range_sum_temp[m] = sums[k]
+                k += 1
+                m += 1
+            range_sum_temp[m] = sums[n]  # left part[n] is smaller
+            m += 1
+
             while i < end and sums[i] - sums[n] < lower:
                 i += 1
             while j < end and sums[j] - sums[n] <= upper:
                 j += 1
-            while k < end and sums[k] < sums[n]:
-                range_sum_temp[m] = sums[k]
-                k += 1
-                m += 1
-            range_sum_temp[m] = sums[n]
-            count += j-i
-        sums[start:k] = range_sum_temp[:k-start]
+            count += j - i
+
+        sums[start:k] = range_sum_temp[:k-start]  # in-place assignment, merge sort complete
         return count
-
-
-
-
-
-
 
 s = Solution()
 print(s.countRangeSum([-2, 5, -1], -2, 2))
-
 
 
 
