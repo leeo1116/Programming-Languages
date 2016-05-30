447a19f0b12fa3e7c9e4fe2a50eb140e0b5bdecb
Charlie <leeo1116@gmail.com>
2016-05-30 16:27:02 -0500

    05/30

--- leetcode/322_coin_change.py
+++ leetcode/322_coin_change.py
@@ -1,3 +1,6 @@
+import timeit
+
+
 class Solution(object):
     # DP
     def coinChange(self, coins, amount):
@@ -17,3 +20,29 @@ class Solution(object):
 
     # Recursion
     def coin_change_dfs(self, coins, amount):
+        if amount < 1:
+            return 0
+        count = [0]*amount
+        return self.dfs_helper(coins, amount, count)
+
+    def dfs_helper(self, coins, r_amount, count):
+        if r_amount < 0:  # Not valid combination
+            return -1
+        if r_amount == 0:  # Complete
+            return 0
+        if count[r_amount-1] != 0:  # the count of r_amount exists, then re-use
+            return count[r_amount-1]
+        min_count = r_amount+1
+        for coin in coins:
+            c_count = self.dfs_helper(coins, r_amount-coin, count)
+            if 0 <= c_count < min_count:
+                min_count = c_count+1
+        count[r_amount-1] = min_count if min_count != r_amount+1 else -1
+        return count[r_amount-1]
+
+
+s = Solution()
+timeit.timeit('s.coinChange([255,196,450,227,98,259,386,36,287,6], 4063)', number=10000)
+timeit.timeit('s.coin_change_dfs([255,196,450,227,98,259,386,36,287,6], 4063)', number=10000)
+print(s.coinChange([255,196,450,227,98,259,386,36,287,6], 4063))
+print(s.coin_change_dfs([255,196,450,227,98,259,386,36,287,6], 4063))
