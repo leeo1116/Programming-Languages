82e2d287db7a4c52f27667a86beef1a46e1b442f
Charlie <leeo1116@gmail.com>
2016-06-05 23:43:03 -0500

    06/05

--- leetcode/216_combination_sum_III.py
+++ leetcode/216_combination_sum_III.py
@@ -5,8 +5,24 @@ class Solution(object):
         :type n: int
         :rtype: List[List[int]]
         """
+        combination = []
+        return self.combination_sum_helper(k, n, 1, combination)
+
+    def combination_sum_helper(self, k, n, start, combination):
+        if n == start:
+            return [[start]]
+        if k == 0:
+            return [[]]
+
+        for i in range(start, 10):
+            if k:
+                comb = self.combination_sum_helper(k-1, n-i, i+1, combination)
+            else:
+                comb = self.combination_sum_helper(k, n, i+1, combination)
+            for c in comb:
+                combination.append([i]+c)
+        return combination
 
 
-    def combination_sum_helper(self, k, n, combination):
-        if n == 0:
-            combination.append
+s = Solution()
+print(s.combinationSum3(2, 7))
