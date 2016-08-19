3b6170155b84da7a95d428f55103e01d11727b13
leeo1116 <leeo1116@gmail.com>
2016-08-18 18:08:10 -0700

    08/18

--- Projects/parser/example.py
+++ Projects/parser/example.py
@@ -7,7 +7,9 @@ parser.add_argument("square", type=int,
                     help="Display a square of a given number")
 parser.add_argument("-v", "--verbosity", action="count", default=3,
                     help="increase output verbosity")  # type=int, choices=[0, 1, 2],
-#parser.add_argument("--verbose", help="increase output verbosity", action="store_true")
+
+# parser.add_argument("--verbose", help="increase output verbosity", action="store_true")
+
 # start parsing arguments typed in command line, return the arguments added as a Namespace class?
 args = parser.parse_args()
 print(args.verbosity)
